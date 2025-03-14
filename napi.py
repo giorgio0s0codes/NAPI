943598a4df07aee4fef2f2703c135f541015f7d1
giorgio0s0codes <giorgio.oso@outlook.com>
2025-03-14 11:24:13 -0600

    second version actually displays the tittles of the articles found, need to clean it and make a new readme.

--- napi.py
+++ napi.py
@@ -5,10 +5,25 @@ import json
 
 newsapi = NewsApiClient(api_key='f860d73b9e554ef49696f6ec93350997')
 
-data = newsapi.get_everything(q='jupyter lab', language='en', page_size=20)
+data = newsapi.get_everything(q='Canva', language='en', page_size=20)
+
+#This will return dict, wich I've read that means the info is in type dictionary.
+#print(type(data))
 type(data)
-print(type(data))
-#data.keys()
+
+#This returns dict_keys(['status', 'totalResults', 'articles']), which gives structure to the data.
+data.keys()
+
+#This returns the status of the request to the news.
 #data['status']
+
+#This returns the number of articles you actually found, not the number of the ones you asked for.
 #data['totalResults']
-#type[data['articles']]
+
+#I used this to discover what was 'articles', it's a list.
+#print(type(data['articles']))
+
+#print(data['articles'][0])
+
+for article in data['articles']:
+    print(article['title'])
