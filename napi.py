eb379acbd356ec73d862a6a47906f84796021193
giorgio0s0codes <giorgio.oso@outlook.com>
2025-03-14 15:22:44 -0600

    4.0 this version can now display more info on a selected article, as a next step I'll discern the importance of the appearing news.

--- napi.py
+++ napi.py
@@ -3,12 +3,17 @@ import datetime as dt
 import pandas as pd
 import json
 
+#This is where the user places their api Key in ''.
 newsapi = NewsApiClient(api_key='f860d73b9e554ef49696f6ec93350997')
 
-data = newsapi.get_everything(q='Canva', language='en', page_size=20)
+data = newsapi.get_everything(q='Ibero', language='es', page_size=20)
 
 articles = data['articles']
 
 #This loop actually displays the names of the articles it found.
 for x, y in enumerate(articles):
     print(f'{x} {y["title"]}')
+
+#This loop displays more info on a particular selected article.
+for key, value in articles[3].items():
+    print(f"\n{key.ljust(15)}{value}")
