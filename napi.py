a5c2c9ff65ac739f70f130e037fdffe7cb2733ca
giorgio0s0codes <giorgio.oso@outlook.com>
2025-03-14 14:53:23 -0600

    3.0 This version can now search the database and receive the name of articles relating to a specific search word.

--- napi.py
+++ napi.py
@@ -7,23 +7,8 @@ newsapi = NewsApiClient(api_key='f860d73b9e554ef49696f6ec93350997')
 
 data = newsapi.get_everything(q='Canva', language='en', page_size=20)
 
-#This will return dict, wich I've read that means the info is in type dictionary.
-#print(type(data))
-type(data)
+articles = data['articles']
 
-#This returns dict_keys(['status', 'totalResults', 'articles']), which gives structure to the data.
-data.keys()
-
-#This returns the status of the request to the news.
-#data['status']
-
-#This returns the number of articles you actually found, not the number of the ones you asked for.
-#data['totalResults']
-
-#I used this to discover what was 'articles', it's a list.
-#print(type(data['articles']))
-
-#print(data['articles'][0])
-
-for article in data['articles']:
-    print(article['title'])
+#This loop actually displays the names of the articles it found.
+for x, y in enumerate(articles):
+    print(f'{x} {y["title"]}')
